b159ffe37e27b335b0d761f750943977132e4c7c
敖其尔 <453729407@qq.com>
2019-08-31 21:17:19 +0800

    黑马头条PC-登录模块-页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-      Login
+  <div class="login">
+<el-card class="box-card">
+    <div><img src="../../assets/img/logo_index.png" alt=""></div>
+    <el-form ref="form" :model="form">
+  <el-form-item>
+    <el-input placeholder="请输入手机号码"></el-input>
+  </el-form-item>
+  <el-form-item>
+    <el-input style='width:260px' class="input" placeholder="请输入验证码"></el-input>
+    <el-button style='float:right'>发送验证码</el-button>
+  </el-form-item>
+  <el-form-item>
+     <el-checkbox class="check">我已阅读并同意用户协议和隐私条款</el-checkbox>
+  </el-form-item>
+   <el-form-item>
+<el-button type="primary" class="btn">确定</el-button>
+  </el-form-item>
+  </el-form>
+</el-card>
+
  </div>
 </template>
 
@@ -10,6 +28,33 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.login{
+    width: 100%;
+    height:100vh;
+    background:url(../../assets/img/login_bg.jpg);
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+        align-items: center;
+    .box-card{
+        width: 430px;
+        height: 360px;
+        text-align: center;
+        img{
+            height: 45px;
+            margin-bottom: 40px;
+        }
 
+    }
+    .input{
+        float: left;
+    }
+    .check{
+        float: left;
+    }
+    .btn{
+        width: 100%;
+    }
+}
 </style>
