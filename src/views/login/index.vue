200fa3e7b66b2fce686c6bf22f16cb097cae6b92
敖其尔 <453729407@qq.com>
2019-09-01 08:32:49 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,21 +2,21 @@
   <div class="login">
 <el-card class="box-card">
     <div><img src="../../assets/img/logo_index.png" alt=""></div>
-    <el-form ref="form" :model="form">
-  <el-form-item>
-    <el-input placeholder="请输入手机号码"></el-input>
+<el-form ref="form" :model="loginForm" :rules="loginRules" :status-icon="true">
+  <el-form-item prop="mobile">
+    <el-input v-model="loginForm.mobile" placeholder="请输入手机号码"></el-input>
   </el-form-item>
-  <el-form-item>
-    <el-input style='width:260px' class="input" placeholder="请输入验证码"></el-input>
+  <el-form-item prop="code">
+    <el-input v-model="loginForm.code" style='width:260px' class="input" placeholder="请输入验证码"></el-input>
     <el-button style='float:right'>发送验证码</el-button>
   </el-form-item>
-  <el-form-item>
-     <el-checkbox class="check">我已阅读并同意用户协议和隐私条款</el-checkbox>
+  <el-form-item prop="check">
+     <el-checkbox v-model="loginForm.check" class="checke">我已阅读并同意用户协议和隐私条款</el-checkbox>
   </el-form-item>
    <el-form-item>
 <el-button type="primary" class="btn">确定</el-button>
   </el-form-item>
-  </el-form>
+</el-form>
 </el-card>
 
  </div>
@@ -24,7 +24,40 @@
 
 <script>
 export default {
-
+  data () {
+    let validator = function (rule, value, callback) {
+      if (value) {
+        callback()
+      } else {
+        callback(new Error('您必须选择同意条款才能继续'))
+      }
+    }
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      loginRules: { mobile: [{
+        required: true,
+        message: '手机号不能为空'
+      }, {
+        pattern: /^1[3456789]\d{9}$/,
+        message: '请输入正确的手机号码'
+      }],
+      code: [{
+        required: true,
+        message: '验证码不能为空'
+      }, {
+        pattern: /^\d{6}$/,
+        message: '请输入正确的验证码'
+      }],
+      check: [{
+        validator
+      }]
+      }
+    }
+  }
 }
 </script>
 
@@ -50,7 +83,7 @@ export default {
     .input{
         float: left;
     }
-    .check{
+    .checke{
         float: left;
     }
     .btn{
