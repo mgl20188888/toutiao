09aca3669272bd8c9617b9aef7b732becbefcdad
敖其尔 <453729407@qq.com>
2019-09-02 16:46:03 +0800

    黑马头条PC-主页模块-头部信息查询2

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import Elementui from 'element-ui'
+import Component from './components/index'
 import 'element-ui/lib/theme-chalk/index.css'
 import './style/index.less'
 import axios from 'axios'
@@ -9,6 +10,7 @@ axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 Vue.use(Elementui)
+Vue.use(Component)
 new Vue({
   router,
   render: h => h(App)
