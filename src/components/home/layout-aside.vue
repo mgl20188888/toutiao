a5884c6fac38dc579b6c0b8ef9c7e28d3345fc6d
敖其尔 <453729407@qq.com>
2019-09-02 11:04:57 +0800

    黑马头条PC-主页模块-左侧导航菜单

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="logo1">
+        <div class="m">
+            <img src="../../assets/img/logo_admin.png" alt="">
+        </div>
+
+         <el-menu
+      background-color="#353b4e"
+      text-color="#adafb5"
+      active-text-color="#ffd04b">
+
+      <el-menu-item index="2">
+        <i class="el-icon-s-home"></i>
+        <span slot="title">首页</span>
+      </el-menu-item>
+
+      <el-submenu class="a">
+          <template slot="title">
+        <i class="el-icon-s-order"></i>
+        <span slot="title">内容列表</span>
+          </template>
+          <el-menu-item>发布文章</el-menu-item>
+          <el-menu-item>内容列表</el-menu-item>
+          <el-menu-item>评论管理</el-menu-item>
+          <el-menu-item>素材管理</el-menu-item>
+      </el-submenu>
+      <el-submenu class="b">
+          <template slot="title">
+        <i class="el-icon-s-custom"></i>
+        <span slot="title">粉丝管理</span>
+          </template>
+          <el-menu-item>图文数据</el-menu-item>
+          <el-menu-item>粉丝概况</el-menu-item>
+          <el-menu-item>粉丝列表</el-menu-item>
+          <el-menu-item>粉丝画像</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3">
+        <i class="el-icon-s-tools"></i>
+        <span slot="title">账户信息</span>
+      </el-menu-item>
+
+    </el-menu>
+    </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.logo1{
+    width: 200px;
+    height: 100vh;
+    background-color:#353b4e ;
+    .m{
+        padding: 10px 0;
+        background-color: #2e2f32;
+        text-align: center;
+
+        img{
+        height: 30px;
+
+    }
+    }
+
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
-      home
+ <el-container>
+  <el-aside width="200px">
+    <aside-a>
+      <layout-aside></layout-aside>
+    </aside-a>
+  </el-aside>
+  <el-container>
+    <el-header>头部</el-header>
+    <el-main>整体</el-main>
+  </el-container>
+</el-container>
   </div>
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
-
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
 
 <style lang="less" scoped>
+
 </style>
