cd31f8002f1e59b6aeed8a94426d88f7e0089010
敖其尔 <453729407@qq.com>
2019-09-05 14:56:24 +0800

    黑马头条PC-评论列表-页面结构-table的使用

--- src/views/comunt/index.vue
+++ src/views/comunt/index.vue
@@ -1,11 +1,45 @@
 <template>
-  <div>
-     评论列表
-  </div>
+  <el-card>
+    <bread-crumb slot="header">
+    <template slot="title">评论列表</template>
+    </bread-crumb>
+    <el-table
+      :data="list"
+      style="width: 100%">
+      <el-table-column
+        prop="name"
+        label="标题"
+        width="600">
+      </el-table-column>
+      <el-table-column
+        prop="zhuangt"
+        label="评论状态"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="zong"
+        label="评论总数">
+      </el-table-column>
+      <el-table-column
+        prop="fensping"
+        label="粉丝评论数">
+      </el-table-column>
+      <el-table-column
+        prop="caozuo"
+        label="操作">
+      </el-table-column>
+    </el-table>
+
+  </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      list: []
+    }
+  }
 
 }
 </script>
