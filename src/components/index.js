b434332f842e59248c53e18287f8d0dc77506f8b
敖其尔 <453729407@qq.com>
2019-09-05 13:51:53 +0800

    黑马头条PC-面包屑组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import layoutAside from './home/layout-aside.vue'
 import layoutHeader from './home/layout-header.vue'
+import breadCrumb from './commun/bread-crumb.vue'
 
 export default {
   install: function (Vue) {
     Vue.component('layout-header', layoutHeader)
     Vue.component('layout-aside', layoutAside)
+    Vue.component('bread-crumb', breadCrumb)
   }
 }
