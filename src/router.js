7af3df0fcc7a1e0cc1e065582978b12e52f6fbc7
敖其尔 <453729407@qq.com>
2019-09-07 08:38:39 +0800

    黑马头条PC-内容列表-新建页面-挂载路由

--- src/router.js
+++ src/router.js
@@ -6,39 +6,39 @@ import Main from './views/home/main.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/login',
-      component: Login
+  routes: [{
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home,
+    children: [{
+      path: '',
+      component: Main
     },
     {
-      path: '/',
-      redirect: '/home'
+      path: '/home/comment',
+      component: () =>
+                        import('./views/comunt')
+    }, {
+      path: '/home/material',
+      component: () =>
+                        import('./views/sucai')
     },
     {
-      path: '/home',
-      name: 'home',
-      component: Home,
-      children: [{
-        path: '',
-        component: Main
-      },
-      {
-        path: '/home/comment',
-        component: () => import('./views/comunt')
-      }, {
-        path: '/home/material',
-        component: () => import('./views/sucai')
-      }]
+      path: '/home/articles',
+      component: () =>
+                        import('./views/neirong')
     }
 
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
+    ]
+  }
   ]
+
 })
