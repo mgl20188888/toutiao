ff7ea3a9bc7e537636c54faaf2f64eb19d7e0788
敖其尔 <453729407@qq.com>
2019-09-08 16:09:45 +0800

    黑马头条PC-内容列表-页面结构-分页请求

--- src/views/neirong/index.vue
+++ src/views/neirong/index.vue
@@ -14,15 +14,14 @@
         </el-radio-group>
       </el-form-item>
       <el-form-item label="频道列表">
-
         <el-select @change="changeCondition" v-model="searchForm.channel_id">
           <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-form-item>
-      <el-form-item label="日期选择">
+      <el-form-item label="日期选择:">
         <el-date-picker
-        @change="changeCondition"
-        value-format="yyyy-mm-dd"
+          @change="changeCondition"
+          value-format="yyyy-MM-dd"
           v-model="searchForm.dateRange"
           type="daterange"
           start-placeholder="开始日期"
@@ -30,14 +29,14 @@
         ></el-date-picker>
       </el-form-item>
     </el-form>
-    <div class="total">符合条件的有1489条信息</div>
+    <div class="total">符合条件的有{{page.total}}条信息</div>
     <div class="art-left">
       <div class="left1" v-for="(item,index) in list" :key="index">
         <div class="left">
           <img :src="item.cover.images.length ? item.cover.images[0]:defaultImg" alt />
           <div class="left2">
             <span class="nei">{{item.title}}</span>
-            <el-tag style="width:60px">{{item.status | statusText}}</el-tag>
+            <el-tag v-bind:type="item.status | statusType " style="width:60px">{{item.status | statusText}}</el-tag>
             <span class="riqi">{{item.pubdate}}</span>
           </div>
         </div>
@@ -51,6 +50,16 @@
         </div>
       </div>
     </div>
+    <el-row type="flex" justify="center" style="margin-top:20px">
+      <el-pagination
+        @current-change="pageChange"
+        :current-page="page.page"
+        :page-size="page.pageSize"
+        :total="page.total"
+        background
+        layout="prev, pager, next"
+      ></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -63,19 +72,35 @@ export default {
       searchForm: {
         status: 5,
         channel_id: null,
-        dateRange: ''
+        dateRange: []
       },
-      channels: []
+      channels: [],
+      page: {
+        page: 1,
+        pageSize: 10,
+        total: 0
+      }
     }
   },
   methods: {
+    pageChange (newPage) {
+      this.page.page = newPage
+      this.getConditionArtical()
+    },
 
     changeCondition () {
+      this.page.page = 1
+      this.getConditionArtical()
+    },
+
+    getConditionArtical () {
       let params = {
         status: this.searchForm.status === 5 ? null : this.searchForm.status,
         channel_id: this.searchForm.channel_id,
         begin_pubdate: this.searchForm.dateRange.length > 0 ? this.searchForm.dateRange[0] : null,
-        end_pubdate: this.searchForm.dateRange.length > 1 ? this.searchForm.dateRange[1] : null
+        end_pubdate: this.searchForm.dateRange.length > 1 ? this.searchForm.dateRange[1] : null,
+        page: this.page.page,
+        per_page: this.page.pageSize
       }
       this.getArticle(params)
     },
@@ -86,6 +111,7 @@ export default {
         params
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     },
     getChannels () {
@@ -115,25 +141,38 @@ export default {
         default:
           break
       }
+    },
+    statusType (value) {
+      switch (value) {
+        case 0:
+          return 'warning'
+        case 1:
+          return 'info'
+        case 2:
+          return 'success'
+        case 3:
+          return 'danger'
+        default:
+          break
+      }
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-.total{
+.total {
   color: #333;
   padding: 20px;
   border-bottom: 1px solid #ccc;
   font-size: 14px;
 }
 .art-left {
-
   .left1 {
-     display: flex;
-     justify-content: space-between;
-     padding: 10px 0;
-     border-bottom: 1px dashed #ccc;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 0;
+    border-bottom: 1px dashed #ccc;
     .left {
       display: flex;
       justify-content: space-between;
@@ -142,25 +181,25 @@ export default {
         height: 100px;
         border-radius: 4px;
       }
-      .left2{
+      .left2 {
         margin-left: 15px;
         display: flex;
         flex-direction: column;
         justify-content: space-around;
-        .nei{
+        .nei {
           font-size: 14px;
           color: #333;
         }
-        .riqi{
+        .riqi {
           font-size: 12px;
           color: #999;
         }
       }
     }
   }
-  .right{
+  .right {
     font-size: 12px;
-    span{
+    span {
       margin-right: 5px;
       cursor: pointer;
     }
